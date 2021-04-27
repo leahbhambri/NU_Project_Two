9866316f1f25f28bfbb86a1904360785ed22fa37
Hilary <hilary.koerner@gmail.com>
2021-04-27 18:28:48 -0500

    changed path to API route for app.js

--- Project_Documents/static/app.js
+++ Project_Documents/static/app.js
@@ -157,7 +157,7 @@ function makeResponsive() {
     }
 
     // Import Data from the d.csv File & Execute Everything Below
-    d3.csv("Resources/merge_df.csv")
+    d3.json('http://127.0.0.1:5000/api/v1.0/water_data')
         .then(function (acsData) {
             console.log(acsData)
             // Format/Parse the Data (Cast as Numbers)

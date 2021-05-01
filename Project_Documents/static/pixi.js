2d478bcc6e29e152ffa4216fb876ff812bf16ba0
Hilary <hilary.koerner@gmail.com>
2021-05-01 11:38:27 -0500

    pixi js

--- Project_Documents/static/pixi.js
+++ Project_Documents/static/pixi.js
@@ -1,25 +1,25 @@
-var app; 
+// var app; 
 
-        function initPixi() {
-            app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-            // document.body.appendChild(app.view);
-            document.body.hero.appendChild(app.view);
-            var image = new PIXI.Sprite.from("images/CleanvsDirtyWater.jpg");
-            image.width = window.innerWidth;
-            // image.height = window.innerHeight;
-            image.height = "500px";
-            app.stage.addChild(image);
+//         function initPixi() {
+//             app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+//             // document.body.appendChild(app.view);
+//             document.body.appendChild(app.view);
+//             var image = new PIXI.Sprite.from("images/CleanvsDirtyWater.jpg");
+//             image.width = window.innerWidth;
+//             // image.height = window.innerHeight;
+//             image.height = "500px";
+//             app.stage.addChild(image);
 
-            displacementSprite = new PIXI.Sprite.from("images/Clouds.jpg");
-            displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-            displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-            app.stage.addChild(displacementSprite);
-            app.stage.filters = [displacementFilter];
-            animate();
-        }
-        function animate() {
-        displacementSprite.x += 10;
-        displacementSprite.y += 4;
-        requestAnimationFrame(animate);
-    }
-    initPixi();
+//             displacementSprite = new PIXI.Sprite.from("images/Clouds.jpg");
+//             displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+//             displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+//             app.stage.addChild(displacementSprite);
+//             app.stage.filters = [displacementFilter];
+//             animate();
+//         }
+//         function animate() {
+//         displacementSprite.x += 10;
+//         displacementSprite.y += 4;
+//         requestAnimationFrame(animate);
+//     }
+//     initPixi();
